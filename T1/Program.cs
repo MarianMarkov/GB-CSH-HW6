207696470590b7801fc334dca1b538c9d475bc30
Марьян Марков <alchnichort@gmail.com>
2023-01-28 00:28:28 +0300

    fc

---
+++ T1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите числа через пробел:");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Чисел больше нуля: {count}");
---
+++ T2/Program.cs
@@ -0,0 +1,13 @@
+﻿float b1, b2, k1, k2, x, y;
+Console.Write("Введите b1: ");
+b1 = float.Parse(Console.ReadLine()!);
+Console.Write("Введите k1: ");
+k1 = float.Parse(Console.ReadLine()!);
+Console.Write("Введите b2: ");
+b2 = float.Parse(Console.ReadLine()!);
+Console.Write("Введите k2: ");
+k2 = float.Parse(Console.ReadLine()!);
+
+x = (b2 - b1) / (k1 - k2);
+y = k2 * x + b2;
+Console.WriteLine($"Точка пересечения [{x};{y}]");
