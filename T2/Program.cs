c3bd82aacda147e8c4ebcde7c2695c7fd3d850dc
Марьян Марков <alchnichort@gmail.com>
2023-01-28 12:36:35 +0300

    sc

--- T2/Program.cs
+++ T2/Program.cs
@@ -8,6 +8,13 @@ b2 = float.Parse(Console.ReadLine()!);
 Console.Write("Введите k2: ");
 k2 = float.Parse(Console.ReadLine()!);
 
-x = (b2 - b1) / (k1 - k2);
-y = k2 * x + b2;
-Console.WriteLine($"Точка пересечения [{x};{y}]");
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые коллинеарны");
+}
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k2 * x + b2;
+    Console.WriteLine($"Точка пересечения [{x};{y}]");
+}
